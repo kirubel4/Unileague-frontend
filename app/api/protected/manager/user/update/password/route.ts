9d772cc16bd3fafa6a5222051f3d92a0feb8271a
kirubel4 <kirubelayele2127@gmail.com>
2026-01-16 12:48:53 +0300

    change password

--- app/api/protected/manager/user/update/password/route.ts
+++ app/api/protected/manager/user/update/password/route.ts
@@ -1,39 +1,44 @@
-import { ApiResponse } from "@/lib/utils";
-import { NextRequest, NextResponse } from "next/server";
+import { ApiResponse } from '@/lib/utils';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
-    const mid = request.cookies.get("mid")?.value;
-    if (!mid) {
-      return NextResponse.json({ message: "Missing id" }, { status: 400 });
+    const username = request.cookies.get('uName')?.value;
+    if (!username) {
+      return NextResponse.json(
+        { message: 'Missing username' },
+        { status: 400 }
+      );
     }
-    const body = await request.json();
+
+    const { currentPassword, newPassword } = await request.json();
+
     const backend = process.env.NEXT_PUBLIC_BACKEND_URL;
     if (!backend) {
       return NextResponse.json(
-        { message: "Missing NEXT_PUBLIC_BACKEND_URL" },
+        { message: 'Missing NEXT_PUBLIC_BACKEND_URL' },
         { status: 500 }
       );
     }
-    const res = await fetch(`${backend}/auth/user/update`, {
-      method: "POST",
+    const res = await fetch(`${backend}/auth/changePassword`, {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
+
       body: JSON.stringify({
-        id: mid,
-        data: body, // ✅ MATCHES backend
+        username: username,
+        currentPassword: currentPassword,
+        newPassword: newPassword,
       }),
     });
 
-    const data: ApiResponse = await res.json();
-    if (data.success) {
-    }
+    const data = await res.json();
     return NextResponse.json(data, { status: res.status });
   } catch (error) {
-    console.error("Proxy create error:", error);
+    console.error('Proxy create error:', error);
     return NextResponse.json(
-      { message: "Internal Server Error", error: String(error) },
+      { message: 'Internal Server Error', error: String(error) },
       { status: 500 }
     );
   }
